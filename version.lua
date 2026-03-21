53bfce2075581987ac5137f3435f7071b85f77ef
CrazySpottedDove <3230104178@zju.edu.cn>
2026-03-21 19:29:18 +0800

    迁移存档位置

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 local v = (arg[2] == "debug" or arg[2] == "release") and "DEBUG" or "RELEASE"
 
 local version = {
-	identity = "kingdom_rush",
+	identity = "kingdom_rush_dove",
 	title = "王国保卫战 dove 版",
 	string = "kr1-desktop-5.6.12",
 	string_short = "5.6.12",
@@ -9,7 +9,7 @@ local version = {
 	vc = "kr1-desktop-5.6.12",
 	build = v,
 	bundle_keywords = "-standalone",
-	id = "9.7.9"
+	id = "9.8.0"
 }
 
 return version
